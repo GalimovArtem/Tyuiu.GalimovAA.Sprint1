1dff65e2f032645df34cad999e87b86c23910799
Tyuiu.GalimovAA@outlook.com <Tyuiu.GalimovAA@outlook.com>
2024-09-29 18:03:21 +0500

    Task1

---
+++ Tyuiu.GalimovAA.Sprint1.Task1.V27.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.GalimovAA.Sprint1.Task1.V27.Lib;
+
+namespace Tyuiu.GalimovAA.Sprint1.Task1.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 2.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(1.5, res);
+        }
+    }
+}
