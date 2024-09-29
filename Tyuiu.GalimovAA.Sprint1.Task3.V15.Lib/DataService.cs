2a298b0ffce245fddb28da465c1f873d91a32913
Tyuiu.GalimovAA@outlook.com <Tyuiu.GalimovAA@outlook.com>
2024-09-29 19:12:06 +0500

    Task3

--- Tyuiu.GalimovAA.Sprint1.Task3.V15.Lib/DataService.cs
+++ Tyuiu.GalimovAA.Sprint1.Task3.V15.Lib/DataService.cs
@@ -7,11 +7,8 @@ namespace Tyuiu.GalimovAA.Sprint1.Task3.V15.Lib
     {
         public double DistanceOverTime(double v1, double v2, double S, double T)
         {
-            double traveledDistance = (v1 + v2) * T;
-
-            double newDistance = S - traveledDistance;
-
-            return Math.Round(Math.Max(0, Math.Abs(newDistance)), 3);
+            double distance = S + (v1 + v2) * T;
+            return Math.Round(Math.Max(0, distance), 3);
         }
     }
 }
