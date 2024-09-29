70081d081f80e476ea65a3662d9c08e66c404c77
Tyuiu.GalimovAA@outlook.com <Tyuiu.GalimovAA@outlook.com>
2024-09-29 17:47:20 +0500

    Task1

---
+++ Tyuiu.GalimovAA.Sprint1.Task0.V27.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.GalimovAA.Sprint1.Task0.V27.Lib
+{
+    public class DataService : ISprint1Task1V27
+    {
+        public double Calculate(double x, double y)
+        {
+            return (x + y) / 2;
+        }
+    }
+}
