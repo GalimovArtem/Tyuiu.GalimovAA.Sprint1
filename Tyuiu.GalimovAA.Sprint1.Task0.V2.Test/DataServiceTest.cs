d374f14239c1110470e944a8810f8bca941a90e4
Tyuiu.GalimovAA@outlook.com <Tyuiu.GalimovAA@outlook.com>
2024-09-29 17:28:45 +0500

    Add project files.

---
+++ Tyuiu.GalimovAA.Sprint1.Task0.V2.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.GalimovAA.Sprint1.Task0.V2.Lib;
+
+namespace Tyuiu.GalimovAA.Sprint1.Task0.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(1, res);
+        }
+    }
+}
---
+++ Tyuiu.GalimovAA.Sprint1.Task0.V2/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.GalimovAA.Sprint1.Task0.V2.Lib;
+
+namespace Tyuiu.GalimovAA.Sprint1.Task0.V2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #0 | Выполнил: Галимов А. А. | ПКТб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #0                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #2                                                              *");
+            Console.WriteLine("* Выполнил: Галимов Артём Азатович | ПКТб-24-1                            *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет выражение 3/3*2+12/4/3-2          *");
+            Console.WriteLine("* одинаковых массивов по длине                                            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("* 3/3*2+12/4/3-2                                                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.Calculate());
+
+            Console.ReadKey();
+
+        }
+    }
+}
